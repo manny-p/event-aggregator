b78dd253595e51d06b53cdd69d8b5c7d836e5c92
Manny <parra.manny@gmail.com>
2021-07-22 22:37:23 -0600

    add state

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import axios from 'axios'
 export default function Home() {
+
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+  }, [])
+
+  const call = async () => {
+    const url = ''
+    const response = await axios.get(url)
+    if(response) setData(response)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
